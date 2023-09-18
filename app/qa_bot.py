e5a1a1ca3680b01876147c4644b4a644b1521a5c
mariocbsf <mariocbsf@gmail.com>
2023-09-18 14:18:25 +0000

    remove chroma from indexed files

--- app/qa_bot.py
+++ app/qa_bot.py
@@ -121,7 +121,11 @@ class QABot:
         self.generate_qa_bot()
 
     def generate_qa_bot(self):
-        text = f"""act like a information retrieval of 2 given jsons about mario cardoso experiences, 1 json about professional experiences and other json about academic experiences. answer questions based on information of the given JSONs, dont create or make up an answer. If the information is not in json, explicitly say 'information not available in given content'. |
+        text = f"""act like a information retrieval of 2 given jsons about mario cardoso experiences, 1 json about professional experiences and other json about academic experiences. answer questions based on information of the given JSONs, dont create or make up an answer. If the information is not in json, explicitly say 'information not available in given content'.  |
+        
+        About false positive answers, do not respond something similar to the question if its not directly correlated. If doing so, say it's not the proper answer. Example: if questioned about the projects of Mario, since projects are not listed in given data, You can't say the companies names as answer unless it's said that isn't the proper answer.  |
+        
+        Your answer can have additional complementary information, but the main content must be on the given experiences details. Your answer must be contextualized, natural and cativant. Consider the answer is for for a possible recruiter that wants to hire Mario, so no bullet points only. The answer must include all related data in given document. |
             |
             |
             The professional experiences JSON is an array of experiences, each experience is an object inside the array. each experience contain 5 attributes: |
@@ -148,6 +152,5 @@ class QABot:
 
     def get_answer(self, question):
         answer = self.qa_bot.run(question)
-        print("main stack matches", answer)
         return answer
         
