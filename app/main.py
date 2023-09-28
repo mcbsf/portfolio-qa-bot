b5ed4ead1e79e541ea11870f854e4f533e1a8280
mariocbsf <mariocbsf@gmail.com>
2023-09-28 23:28:54 +0000

    set CORS on fast api

--- app/main.py
+++ app/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from qa_bot import QABot
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class Question (BaseModel):
     question: str
 
